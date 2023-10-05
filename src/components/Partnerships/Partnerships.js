16fee729547d32e47e11bc154d4e0d93d99bbf61
Guyoguyes <g.abduba43@gmail.com>
2023-10-05 10:14:17 +0300

    added pamoja forum

--- src/components/Partnerships/Partnerships.js
+++ src/components/Partnerships/Partnerships.js
@@ -10,8 +10,8 @@ function Partnerships(props) {
                             <h2>Our Partners</h2>
                         </div>
                         <ul className="brand-style-2" style={{width: 'auto'}}>
-                            <l>
-                                <img src="https://res.cloudinary.com/dabfdxbfj/image/upload/v1690653832/regular_y5aooc.png" alt="Logo Images" /> </l>
+                            <li>
+                                <img src="https://res.cloudinary.com/dabfdxbfj/image/upload/v1696488942/PAMOJA_HOPE_LOGO_page-0001_zwnq8t.jpg" alt="Logo Images" /> </li>
                             <li><img src="https://res.cloudinary.com/dabfdxbfj/image/upload/v1690654068/DCD-logo-regular_mtauom.png" alt="Logo Images"/> </li>
                             <li><img src="assets/images/brand/holby.jpg" alt="Logo Images"/> </li>
                         <li>

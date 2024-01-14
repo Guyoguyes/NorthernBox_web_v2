6a1568ec880e5e5f181e60b1e500ae6a97cbdfab
Guyoguyes <g.abduba43@gmail.com>
2024-01-14 19:51:36 +0300

    added blog and events, details of pages not finished

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="*" element={<NotFound />} />
-            <Route path='' element={<Home/>}/>
+            <Route path='/' element={<Home/>}/>
             <Route path='home' element={<Home/>}/>
             <Route path='about' element={<About/>}/>
             <Route path='contact' element={<Contact/>}/>

5975b03ae68850547540b4a8d67f9955cc008774
Guyoguyes <g.abduba43@gmail.com>
2024-01-14 22:25:44 +0300

    fixed navbar and modified the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,64 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Footer() {
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setCurrentYear(new Date().getFullYear());
+        }, 1000); // Update every second to handle year change
+
+        return () => clearInterval(interval);
+    }, []);
     return (
-        <div className="footer-area footer-default">
-            <div className="footer-wrapper">
-                <div className="row align-items-end row--0">
-                    <div className="col-lg-6">
-                        <div className="footer-left">
-                            <div className="inner">
-                                <span>Ready To Do This</span>
-                                <h2>Let's get <br/> to work</h2>
-                                <a className="rn-button-style--2" href="contact">
-                                    <span>Contact Us</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="footer-right" data-black-overlay="6">
-                            <div className="row">
-                                <div className="col-lg-6 col-sm-6 col-12">
-                                    <div className="footer-widget">
-                                        <h4>Quick Link</h4>
-                                        <ul className="ft-link">
-                                            <li><a href="">Membership</a></li>
-                                            <li><a href="">Volunteer</a></li>
-                                            <li><a href="">Let's Talk</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 col-sm-6 col-12 mt_mobile--30">
-                                    <div className="footer-widget">
-                                        <h4>Say Hello</h4>
-                                        <ul className="ft-link">
-                                            <li><a href="mailto:hi@northernbox.org">hi@northernbox.org</a></li>
-                                            <li><a href="mailto:contact@northernbox.org">contact@northernbox.org</a></li>
-                                        </ul>
-
-                                        <div className="social-share-inner">
-                                            <ul className="social-share social-style--2 d-flex justify-content-start liststyle mt--15">
-                                                <li><a href="https://m.facebook.com/groups/2058420990978109/?ref=group_browse"><i className="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="https://www.linkedin.com/company/northernbox/"><i className="fab fa-linkedin-in"></i></a></li>
-                                                <li><a href="https://twitter.com/NorthernBox_"><i className="fab fa-twitter"></i></a></li>
-                                                <li><a href="https://www.instagram.com/northernboxcommunity/"><i className="fab fa-instagram"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-12">
-                                    <div className="copyright-text">
-                                        <p>Copyright © 2023 <a href="home">NorthernBox Community</a>. All Rights Reserved.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <>
+            <footer class="bg-dark rounded-lg shadow m-4 dark:bg-gray-800">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-3  me-4 md:me-6">Copyright © {currentYear} <a href="home">NorthernBox</a>. All Rights Reserved.
+                </span>
+                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="/about" class="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li><a href="/membership" class="hover:underline me-4 md:me-6">Membership</a></li>
+                    {/* <li><a href="" class="hover:underline me-4 md:me-6">Volunteer</a></li> */}
+                    <li><a href="" class="hover:underline me-4 md:me-6">Let's Talk</a></li>
+                    <li><a href="mailto:hi@northernbox.org" class="hover:underline me-4 md:me-6 sm:mt-5">hi@northernbox.org</a></li>
+                    <li><a href="mailto:contact@northernbox.org" class="hover:underline me-4 md:me-6">contact@northernbox.org</a></li>
+                </ul>
+                    {/* <div className="" style={{outline: '1px solid red'}}> */}
+                        <ul className="social-share social-style--2 d-flex justify-content-start liststyle  mt-3">
+                            <li><a href="https://m.facebook.com/groups/2058420990978109/?ref=group_browse"><i className="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://www.linkedin.com/company/northernbox/"><i className="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="https://twitter.com/NorthernBox_"><i className="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.instagram.com/northernboxcommunity/"><i className="fab fa-instagram"></i></a></li>
+                        </ul>
+                    {/* </div> */}
                 </div>
-            </div>
-        </div>
+                
+            </footer>
+
+        </>
+        
     );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,12 @@
 import React, { useState } from 'react';
 import * as AiIcons from 'react-icons/ai';
+import { Transition } from "@headlessui/react";
 
 function Navbar(props) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
-  const toggleSubmenu = (e) => {
-    e.preventDefault();
-    const parentLi = e.currentTarget.parentElement;
-    parentLi.classList.toggle('open');
-    const submenu = parentLi.querySelector('.submenu');
-    if (submenu) {
-      submenu.classList.toggle('active');
-    }
-  };
-
-  const handleNavLinkClick = () => {
-    closeMenu();
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className={`header-area formobile-menu header--transparent ${menuOpen ? 'menu-open' : ''}`}>
+    <header className={`header-area formobile-menu header--transparent`}>
       <div className="header-wrapper" id="header-wrapper">
         <div className="header-left">
           <div className="logo w-72">
@@ -40,35 +19,36 @@ function Navbar(props) {
             </a>
           </div>
         </div>
+        {/*  */}
         <div className="header-right flex items-center">
           <div className="mainmenunav d-lg-block mx-auto">
-            <nav className={`main-menu-navbar ${menuOpen ? 'show' : ''}`}>
+            <nav className={`main-menu-navbar`}>
               <ul className="mainmenu text-xl">
                 <li className="has-droupdown">
-                  <a href="home" onClick={handleNavLinkClick}>
+                  <a href="home" >
                     Home
                   </a>
                 </li>
                 <li>
-                  <a href="about" onClick={handleNavLinkClick}>
+                  <a href="about" >
                     Community
                   </a>
                 </li>
                 <li className="has-droupdown">
-                  <a onClick={toggleSubmenu}>Activities</a>
+                  <a>Activities</a>
                   <ul className="submenu">
                     <li>
-                      <a href="datacamp_donates" onClick={handleNavLinkClick}>
+                      <a href="datacamp_donates" >
                         Datacamp Donate
                       </a>
                     </li>
                     <li>
-                      <a href="events" onClick={handleNavLinkClick}>
+                      <a href="events" >
                         Events
                       </a>
                     </li>
                     <li>
-                      <a href="blogs" onClick={handleNavLinkClick}>
+                      <a href="blogs" >
                         Blogs
                       </a>
                     </li>
@@ -85,7 +65,7 @@ function Navbar(props) {
                   </a>
                 </li> */}
                 <li>
-                  <a href="contact" onClick={handleNavLinkClick}>
+                  <a href="contact" >
                     Contact
                   </a>
                 </li>
@@ -97,18 +77,144 @@ function Navbar(props) {
               <span>Get Started</span>
             </a>
           </div>
-          <div className="humberger-menu d-block d-lg-none pl-4" onClick={toggleMenu}>
-            <span className="menutrigger text-white text-2xl">
-              <AiIcons.AiOutlineMenu />
-            </span>
-          </div>
-          <div className="close-menu d-block d-lg-none pl-4" onClick={closeMenu}>
+          <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          {/* <div className="close-menu d-block d-lg-none pl-4" >
             <span className="closeTrigger text-2xl">
               <AiIcons.AiOutlineClose />
             </span>
-          </div>
+          </div> */}
         </div>
+        {/*  */}
       </div>
+      <Transition
+  show={isOpen}
+  enter="transition ease-out duration-100 transform"
+  enterFrom="opacity-0 scale-95"
+  enterTo="opacity-100 scale-100"
+  leave="transition ease-in duration-75 transform"
+  leaveFrom="opacity-100 scale-100"
+  leaveTo="opacity-0 scale-95"
+>
+  {(ref) => (
+    <div className="md:hidden" id="mobile-menu">
+      <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        {/*  */}
+        <div className="header-right flex items-center">
+          <div className="mainmenunav d-lg-block mx-auto">
+            <nav className={`main-menu-navbar`}>
+              <ul className="mainmenu text-xl">
+                <li className="has-droupdown">
+                  <a href="home" >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="about" >
+                    Community
+                  </a>
+                </li>
+                <li className="has-droupdown">
+                  <a>Activities</a>
+                  <ul className="submenu">
+                    <li>
+                      <a href="datacamp_donates" >
+                        Datacamp Donate
+                      </a>
+                    </li>
+                    <li>
+                      <a href="events" >
+                        Events
+                      </a>
+                    </li>
+                    <li>
+                      <a href="blogs" >
+                        Blogs
+                      </a>
+                    </li>
+                    {/* <li>
+                      <a href="news" onClick={handleNavLinkClick}>
+                        News
+                      </a>
+                    </li> */}
+                  </ul>
+                </li>
+                {/* <li>
+                  <a href="contact" onClick={handleNavLinkClick}>
+                    Hire Talent
+                  </a>
+                </li> */}
+                <li>
+                  <a href="contact" >
+                    Contact
+                  </a>
+                </li>
+                <li>
+                <div className="header-btn ml-auto">
+                  <a className="" href="membership" style={{
+                      backgroundColor: '#007bff',
+                      padding: '10px 20px',
+                      borderRadius: '5px',
+                      color: '#fff',
+                      textDecoration: 'none',
+                    }}>
+                    <span>Get Started</span>
+                  </a>
+                </div>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          
+        </div>
+        {/*  */}
+      </div>
+    </div>
+  )}
+</Transition>
+
     </header>
   );
 }
